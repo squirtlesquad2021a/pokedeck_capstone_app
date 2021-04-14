dc683f27ac8ef00f061574535b353a21edcb56e9
Lex Peterson <lpeterson@chorus.net>
2021-04-14 13:02:57 -0700

    added get controller methods

--- app/controllers/binders_controller.rb
+++ app/controllers/binders_controller.rb
@@ -1,2 +1,15 @@
 class BindersController < ApplicationController
+
+  def index
+    binders = Binder.all
+
+    binder_with_cards = binders.map do |binder|
+      binder_attributes = binder.attributes
+      binder_attributes.merge(card_data: Card.find(binder.card_id))
+    end
+    render json: binder_with_cards
+  end
+
+  
+
 end
