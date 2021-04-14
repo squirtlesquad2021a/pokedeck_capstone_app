211d5246470a88bdbfe671563d1c96fe3f53e181
Lex Peterson <lpeterson@chorus.net>
2021-04-14 15:06:30 -0700

    changed get controller method

--- app/controllers/binders_controller.rb
+++ app/controllers/binders_controller.rb
@@ -5,11 +5,11 @@ class BindersController < ApplicationController
 
     binder_with_cards = binders.map do |binder|
       binder_attributes = binder.attributes
-      binder_attributes.merge(card_data: Card.find(binder.card_id))
+      binder_attributes.merge(card_data: binder.card.attributes)
     end
     render json: binder_with_cards
   end
 
-  
+
 
 end
