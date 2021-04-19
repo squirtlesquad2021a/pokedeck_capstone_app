9aecdfbe959d4ef93a34d768f33e29ae6d082c2c
algtan <agtan@ymail.com>
2021-04-19 14:01:18 -0700

    binder model validations

--- app/models/binder.rb
+++ app/models/binder.rb
@@ -1,4 +1,7 @@
 class Binder < ApplicationRecord
     belongs_to :user
     belongs_to :card
+
+    validates :quantity, presence: true
+    validates_inclusion_of :favorite, in: [true, false]
 end
--- spec/models/binder_spec.rb
+++ spec/models/binder_spec.rb
@@ -1,5 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe Binder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validates binder attributes" do
+    it "quantity is present" do
+      user1 = User.create(email:"email@email.com",password:"password",username:"user1")
+      card1 = Card.create(
+        name: "Alakazam",
+        pokemon_type: "Psychic",
+        set_id: "base1",
+        set_name: "Base",
+        set_series: "Base",
+        number: "1",
+        rarity: "Rare Holo",
+        image: "https://images.pokemontcg.io/base1/1_hires.png",
+        price: 250,
+      )
+      binder1 = Binder.create(
+        user_id: user1.id,
+        card_id: card1.id,
+        favorite: true
+      )
+      expect(binder1.errors[:quantity]).to_not be_empty
+    end
+
+    it "favorite is present" do
+      user1 = User.create(email:"email@email.com",password:"password",username:"user1")
+      card1 = Card.create(
+        name: "Alakazam",
+        pokemon_type: "Psychic",
+        set_id: "base1",
+        set_name: "Base",
+        set_series: "Base",
+        number: "1",
+        rarity: "Rare Holo",
+        image: "https://images.pokemontcg.io/base1/1_hires.png",
+        price: 250,
+      )
+      binder1 = Binder.create(
+        user_id: user1.id,
+        card_id: card1.id,
+        quantity: 1
+      )
+      expect(binder1.errors[:favorite]).to_not be_empty
+    end
+
+    it "are valid" do
+      user1 = User.create(email:"email@email.com",password:"password",username:"user1")
+      card1 = Card.create(
+        name: "Alakazam",
+        pokemon_type: "Psychic",
+        set_id: "base1",
+        set_name: "Base",
+        set_series: "Base",
+        number: "1",
+        rarity: "Rare Holo",
+        image: "https://images.pokemontcg.io/base1/1_hires.png",
+        price: 250,
+      )
+      binder1 = Binder.create(
+        user_id: user1.id,
+        card_id: card1.id,
+        quantity: 1,
+        favorite: false
+      )
+      expect(binder1).to be_valid
+    end
+
+  end
 end
