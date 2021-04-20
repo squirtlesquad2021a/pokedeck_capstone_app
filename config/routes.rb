536e8ebadbcc155f4176ee83a75836fa6787d2fa
algtan <agtan@ymail.com>
2021-04-20 09:37:52 -0700

    deck_price spec working

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   resources :binders
   resources :cards
   devise_for :users
-  get '*path', to: 'home#index', constraints: ->(request){ request.format.html? }
   post '/boosterpack/:user_id' => 'binders#booster_pack'
   post '/dailycard/:user_id' => 'binders#daily_card'
   get '/deckprice/:user_id' => 'binders#deck_price'
   get '/eligibility_check/:user_id' => 'daily_cards#eligibility_check'
+  get '*path', to: 'home#index', constraints: ->(request){ request.format.html? }
   root to: 'home#index'
 end
