9449089b0c2e5dd7fcd1195f690da417b3c99b6e
algtan <agtan@ymail.com>
2021-04-20 15:11:28 -0700

    added dailycard eligibility spec

--- app/javascript/components/mockData.js
+++ app/javascript/components/mockData.js
@@ -12,29 +12,89 @@ let mockCards = [
       price: 250
     },
     {
-        id: 2,
-        name: "Blastoise",
-        pokemon_type: "Water",
-        set_id: "base1",
-        set_name: "Base",
-        set_series: "Base",
-        number: "2",
-        rarity: "Rare Holo",
-        image: "https://images.pokemontcg.io/base1/2_hires.png",
-        price: 399.99
-      },
-      {
-        id: 3,
-        name: "Chansey",
-        pokemon_type: "Basic",
-        set_id: "base1",
-        set_name: "Base",
-        set_series: "Base",
-        number: "3",
-        rarity: "Rare Holo",
-        image: "https://images.pokemontcg.io/base1/3_hires.png",
-        price: 83.98
-      }
+      id: 2,
+      name: "Blastoise",
+      pokemon_type: "Water",
+      set_id: "base1",
+      set_name: "Base",
+      set_series: "Base",
+      number: "2",
+      rarity: "Rare Holo",
+      image: "https://images.pokemontcg.io/base1/2_hires.png",
+      price: 399.99
+    },
+    {
+      id: 3,
+      name: "Chansey",
+      pokemon_type: "Basic",
+      set_id: "base1",
+      set_name: "Base",
+      set_series: "Base",
+      number: "3",
+      rarity: "Rare Holo",
+      image: "https://images.pokemontcg.io/base1/3_hires.png",
+      price: 83.98
+    },
+    {
+      id: 4,
+      name: "Charizard",
+      pokemon_type: "Fire",
+      set_id: "base1",
+      set_name: "Base",
+      set_series: "Base",
+      number: "4",
+      rarity: "Rare Holo",
+      image: "https://images.pokemontcg.io/base1/4_hires.png",
+      price: 1000.0
+    },
+    {
+      id: 5,
+      name: "Clefairy",
+      pokemon_type: "Colorless",
+      set_id: "base1",
+      set_name: "Base",
+      set_series: "Base",
+      number: "5",
+      rarity: "Rare Holo",
+      image: "https://images.pokemontcg.io/base1/5_hires.png",
+      price: 98.99
+    },
+    {
+      id: 6,
+      name: "Gyarados",
+      pokemon_type: "Water",
+      set_id: "base1",
+      set_name: "Base",
+      set_series: "Base",
+      number: "6",
+      rarity: "Rare Holo",
+      image: "https://images.pokemontcg.io/base1/6_hires.png",
+      price: 93.98
+    },
+    {
+      id: 7,
+      name: "Hitmonchan",
+      pokemon_type: "Fighting",
+      set_id: "base1",
+      set_name: "Base",
+      set_series: "Base",
+      number: "7",
+      rarity: "Rare Holo",
+      image: "https://images.pokemontcg.io/base1/7_hires.png",
+      price: 75.0
+    },
+    {
+      id: 8,
+      name: "Machamp",
+      pokemon_type: "Fighting",
+      set_id: "base1",
+      set_name: "Base",
+      set_series: "Base",
+      number: "8",
+      rarity: "Rare Holo",
+      image: "https://images.pokemontcg.io/base1/8_hires.png",
+      price: 0.0
+    }
 
   ]
   export default mockCards
--- spec/requests/daily_cards_spec.rb
+++ spec/requests/daily_cards_spec.rb
@@ -1,7 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe "DailyCards", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /eligibility_check/:user_id" do
+      it 'a new user is not eligible' do
+        #arrange
+        user1 =User.create(
+          email:"userone@noemail.com",
+          password:"password",
+          username:"userone"
+        )
+
+        # sign_in(user1)
+
+        #act
+        get "/eligibility_check/#{user1.id}"
+        #assert
+        response_body = JSON.parse(response.body)
+        expect(response).to have_http_status(200)
+        expect(response_body).to eq false
+      end
+
+      it 'if at least 1 day passes, user is eligible' do
+        #arrange
+        user1 =User.create(
+          email:"userone@noemail.com",
+          password:"password",
+          username:"userone",
+          timestamp_of_last_daily_card: 1.day.ago
+        )
+        #act
+        get "/eligibility_check/#{user1.id}"
+        #assert
+        response_body = JSON.parse(response.body)
+        expect(response).to have_http_status(200)
+        expect(response_body).to eq true
+      end
   end
 end
