1010db35cc88a406d56d0018c33bc6638b760a7f
Lex Peterson <lpeterson@chorus.net>
2021-04-22 13:02:56 -0700

    still trying different solutions

--- app/javascript/components/pages/Splash.js
+++ app/javascript/components/pages/Splash.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react"
 import PokeLogo from '../../../assets/images/PokeLogo2.1.png'
-
-
-
+import ReactHowler from 'react-howler'
+//import Theme from './TitleScreen.mp3'
 
 
 class Splash extends Component{
@@ -10,13 +9,17 @@ render() {
     return(
         <>
         <div className = "splash">
-        <div className= "square">
-            <a href="/home"><img src= {PokeLogo} alt="pokedeckLogo" className= "logo-splash" /></a>
-        </div>    
-        
-        <div className= "orangeSquare">
-        <p className="splashInstructions">(Click Logo to Begin)</p>
-        </div>
+          <div className= "square">
+              <a href="/home"><img src= {PokeLogo} alt="pokedeckLogo" className= "logo-splash" /></a>
+          </div>
+
+          <div className= "orangeSquare">
+            <p className="splashInstructions">(Click Logo to Begin)</p>
+          </div>
+          <ReactHowler
+          src='./pokedeck_capstone_app/app/javascript/components/pages/TitleScreen.mp3'
+          playing={true}
+          />
         </div>
         </>
     )
