8f52ded9eeceddd8c306ee353019f5a795604be5
Lex Peterson <lpeterson@chorus.net>
2021-05-14 01:48:26 -0700

    Responsive Edits

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -4,52 +4,40 @@ import { NavLink } from 'react-router-dom'
 
 import {
     Card, Button, CardImg, CardTitle, CardText, CardDeck,
-    CardSubtitle, CardBody, Container, Row, Col
-  } from 'reactstrap';
-
-
-
-
+    CardSubtitle, CardBody, Container, Row, Col, CardColumns
+  } from 'react-bootstrap';
 class Home extends Component{
   
-
-  
-render() {
-
+  render() {
     const {
         logged_in,
         sign_in_route,
         sign_out_route,
         new_user_route
       } = this.props
-
-      
     return(
-        <>
+      <>
         <h2 className="center">Browse cards</h2>
-        <div className="cardBox" >
-        <Row xs="4">
-        {/* <CardDeck> */}
-        {this.props.cards && this.props.cards.map((card, index) => {
-            return (
-            <Col key = {index} >
-              <Card key = {index} className="cards">
-                  <NavLink to={`/cardshow/${card.id}`}>
-                  <CardImg top width="100%" src={card.image} alt="Card image" />
-                  </NavLink>
-              </Card>
-              <br></br>
-              <br></br>
-            </Col>
-        )})}
-        </Row>
-        {/* </CardDeck> */}
-        </div>
-        </>   
+        <Container fluid>
+            <Row>
+              {this.props.cards && this.props.cards.map((card, index) => {
+                return (
+                  <Col md="3" xs="16" key = {index} >
+                    <Card key = {index} className="cards">
+                        <NavLink to={`/cardshow/${card.id}`}>
+                          <CardImg width="100%" src={card.image} alt="Card image" />
+                        </NavLink>
+                    </Card>
+                    <br></br>
+                    <br></br>
+                </Col>
+                )
+              })}
+            </Row>
+        </Container>
+      </>
     )
-    }
-    }
+  }
+}
   
-
-
 export default Home
